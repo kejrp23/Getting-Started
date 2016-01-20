13184afe2de341cf8376fa5f9b4df266eccf6749
Jason Pittman <j.pittman51@att.net>
2016-01-20 10:19:28 -0500

    Create Promptchange.sh

---
+++ Promptchange.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# I use several environments these days as I'm learning. 
+# One of them doesn't let me save my prompt so I'm creating a script to run
+# that will do it for me here is what I wrote and it didn't work
+# Suggestions?
+
+
+clear
+
+cd /
+
+# This next line never executed
+
+PS1="MyCustomPrompt : "
+
+# This next line did
+
+clear
